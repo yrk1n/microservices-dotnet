528d044ac104f573045bcb4136959eeb45e6f648
yrkn <118745292+yrk1n@users.noreply.github.com>
2024-12-04 23:55:21 +0300

    initial commit and general structure

---
+++ Data/IPlatformRepo.cs
@@ -0,0 +1,14 @@
+using PlatformService.Models;
+
+namespace PlatformService.Data;
+
+public interface IPlatformRepo
+{
+    bool SaveChanges();
+
+    IEnumerable<Platform> GetAllPlatforms();
+    Platform GetPlatformById(int id);
+    void CreatePlatform(Platform plat);
+}
+
+ 
---
+++ Data/PlatformRepo.cs
@@ -0,0 +1,26 @@
+using PlatformService.Models;
+
+namespace PlatformService.Data;
+
+public class PlatformRepo : IPlatformRepo
+{
+    public void CreatePlatform(Platform plat)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Platform> GetAllPlatforms()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Platform GetPlatformById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool SaveChanges()
+    {
+        throw new NotImplementedException();
+    }
+}
